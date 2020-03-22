1d142e16b3663af70631f8ccab1b14b938b15d72
Simon Curtis <s.curtis@uq.net.au>
2020-03-22 14:47:50 +1000

    Start thinking about table implementation

--- netflow-table.c
+++ netflow-table.c
@@ -3,18 +3,50 @@
 
 #include "netflow-table.h"
 
-static struct netflow_table* netflow_table_init (void) {
-   struct netflow_table *table = malloc (sizeof (struct netflow_table));
-   table->placeholder = 0;
-   D("Placeholder got to netflow table init\n");
+#define TABLE_INITIAL_SIZE 2048
+
+/* NOTE: NONE OF THESE ARE THREAD SAFE */
+
+struct netflow_table* netflow_table_init (void) {
+   struct netflow_table *table;
+   
+   D("Initialising NetFlow Table\n");
+   table            = malloc (sizeof (struct netflow_table));
+   table->array     = malloc (sizeof (hashBucket_t) * TABLE_INITIAL_SIZE);
+   table->n_entries = TABLE_INITIAL_SIZE;
+
    return table;
 }
 
 
+void netflow_k_v_from_ring (const char *_p, int len, struct netmap_ring *ring, int cur) {
+
+}
+
+
 /* NOTE: This function isn't threadsafe !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
-static void
-netflow_table_insert(struct netflow_table *table, const char *_p, int len, struct netmap_ring *ring, int cur) {
-   /* Placeholder code */
-   table->placeholder += sizeof (table) + sizeof(_p) + sizeof(len) + sizeof (ring) + cur;
-   D("table placeholder = %d\n", table->placeholder);
+void netflow_table_insert (struct netflow_table *table, key_t key, value_t val) {
+   uint32_t idx = 0;
+
+   idx = hash_crc_4byte (key->proto, idx);
+   idx = hash_crc_4byte (key->ip_src, idx);
+   idx = hash_crc_4byte (key->ip_dst, idx);
+   idx = hash_crc_4byte (key->port_src, idx);
+   idx = hash_crc_4byte (key->port_dst, idx);
+   idx = idx % table->n_entries;
+}
+
+
+void netflow_table_free (struct netflow_table *table) {
+
+}
+
+
+void netflow_table_print (struct netflow_table *table) {
+
+}
+
+
+void netflow_table_print_stats (struct netflow_table *table) {
+
 }
--- netflow-table.h
+++ netflow-table.h
@@ -1,8 +1,18 @@
 #ifndef NETFLOW_TABLE_H
 #define NETFLOW_TABLE_H
+
 struct netflow_table {
-   int placeholder;
+   uint32_t n_entries;
+   hashBucket_t *array; 
 };
+
+typedef struct netflow_table_key {
+    uint8_t proto;
+    uint32_t ip_src;                                /**< saved in network order */
+    uint32_t ip_dst;                                /**< saved in network order */
+    uint16_t port_src;                              /**< saved in network order */
+    uint16_t port_dst;                              /**< saved in network order */
+} key_t;
 
 typedef struct netflow_hashBucket {
     uint8_t vlanId;
@@ -18,4 +28,13 @@ typedef struct netflow_hashBucket {
     
     struct netflow_hashBucket *next;
 } hashBucket_t;
+
+
+/* Functions */
+struct netflow_table* netflow_table_init (void);
+void netflow_table_insert (struct netflow_table *table, const char *_p, int len, struct netmap_ring *ring, int cur);
+void netflow_table_free (struct netflow_table *table);
+void netflow_table_print (struct netflow_table *table);
+void netflow_table_print_stats (struct netflow_table *table);
+
 #endif
